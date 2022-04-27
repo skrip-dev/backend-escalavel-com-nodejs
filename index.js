32f403b4998cd7c4f978d5de5f4b1fb279e7c6f1
Rodrigues Costa <jose@rodriguescosta.com>
2022-04-27 19:33:37 -0300

    feat: add express and receive request

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+import express from 'express'
 import { promises } from 'fs'
 
-const data = await promises.readFile('package.json', {
-  encoding:'utf-8'
-});
+const server = express()
 
-console.log(data)
+
+server.get('/', async (req, res)=>{
+  const data = await promises.readFile('package.json', {
+    encoding:'utf-8'
+  });
+
+  res.send(data)
+})
+
+
+server.listen(3333, ()=>{
+  console.log('server listen')
+})
